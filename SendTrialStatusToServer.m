c0751904444cc42b461d2b728104db47599a038b
Torben Ott <tott@cshl.edu>
2017-01-23 14:59:31 -0500

    Added subject and protocol options.

--- SendTrialStatusToServer.m
+++ SendTrialStatusToServer.m
@@ -1,11 +1,22 @@
-function SendTrialStatusToServer(script,rig,outcome)
+function SendTrialStatusToServer(script,rig,outcome,subject,protocol)
 %Calls PHP script at HTTP location specified in SCRIPT [string].
 %Gives two variables: RIG [string] name and OUTCOME [numerical vector of integers from 1-9, max length 30].
 %Torben Ott, CSHL, 2017
+if nargin<5
+    protocol='na';
+end
+if nargin<4
+    subject='na';
+end
+if nargin<3
+    error('SendTrialStatusToServer:Missing input argument.');
+end
+    
 if length(outcome)>30
     outcome=outcome(end-29:end);
 end
-length(outcome)
-url=strcat('http://kepecsdata.cshl.edu/scripts/',script,'?rig=',rig,'&outcome=',num2str(outcome,'%i'));
+outcome(isnan(outcome)) = 9;
+
+url=strcat('http://kepecsdata.cshl.edu/scripts/',script,'?rig=',rig,'&outcome=',num2str(outcome,'%i'),'&subject=',subject,'&protocol=',protocol);
 webread(url);
 end
